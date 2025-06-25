b07d505b003db6f8b5c7894c7446487ece09bd8d
Javlon Talipov <javlon.talipov13@mail.ru>
2025-06-25 16:47:00 +0500

    add getters for attack and defence modifiers

--- ship/modifiers/modifiers.py
+++ ship/modifiers/modifiers.py
@@ -13,19 +13,16 @@ class ModifierType(Enum):
 class Modifiers:
     # todo упростить это "явление" кода
     def __init__(self, modifiers):
-        self.attack_modifiers = []
-        self.defence_modifiers = []
+        self._attack_modifiers = []
+        self._defence_modifiers = []
         self._set_modifiers_from(modifiers)
-
-    def get_modifier_fn(self, fn_name):
-        return get_modifier_from_name(fn_name)
 
     def add_modifier(self, fn_name, modifier_type=ModifierType.ATTACK_MODIFIERS):
         try:
-            target = self.get_modifier_fn(fn_name)
-            index = bin_search(self[modifier_type.value], target,
+            target = self._get_modifier_fn(fn_name)
+            index = bin_search(self[f'_{modifier_type.value}'], target,
                                lambda m, arr, target: arr[m]['priority'] > target['priority'])
-            self[modifier_type.value].insert(index, target)
+            self[f'_{modifier_type.value}'].insert(index, target)
         except ValueError as e:
             print(e)
         except Exception as e:
@@ -37,6 +34,20 @@ class Modifiers:
                 self[modifier_type.value].remove(fn)
                 return index
         raise ValueError(f'{type(self).__name__}: Modifier {fn_name} in {modifier_type.value} not found')
+
+    @property
+    def attack_modifiers(self):
+        return self._get_modifiers_fn(ModifierType.ATTACK_MODIFIERS)
+
+    @property
+    def defence_modifiers(self):
+        return self._get_modifiers_fn(ModifierType.DEFENCE_MODIFIERS)
+
+    def _get_modifiers_fn(self, modifier_type):
+        return [modifier['modifier'] for modifier in self[f'_{modifier_type.value}']]
+
+    def _get_modifier_fn(self, fn_name):
+        return get_modifier_from_name(fn_name)
 
     def _set_modifiers_from(self, input_modifiers):
         raw_modifiers = self._get_raw_modifiers(input_modifiers)
@@ -56,10 +67,10 @@ class Modifiers:
         return getattr(self, value)
 
     def _extract_fns_name(self, fns, separator=', '):
-        return separator.join([fn.__name__ for fn in fns])
+        return separator.join([str((fn['modifier'].__name__, fn['priority'].value)) for fn in fns])
 
     def __repr__(self):
-        return f'Modifiers: \n attack_modifiers: {self._extract_fns_name(self.attack_modifiers)} \n defence_modifiers: {self._extract_fns_name(self.defence_modifiers)}'
+        return f'Modifiers: \n attack_modifiers: {self._extract_fns_name(self._attack_modifiers)} \n defence_modifiers: {self._extract_fns_name(self._defence_modifiers)}'
 
 
 if __name__ == "__main__":
@@ -68,10 +79,8 @@ if __name__ == "__main__":
         "defence_modifiers": ["german_ships"]
     }
     modifiers = Modifiers(example)
+    print(modifiers)
     # modifiers.add_modifier('british_ships')
     # modifiers.add_modifier('cruiser')
-    modifiers.add_modifier('bismark_hood', ModifierType.DEFENCE_MODIFIERS)
     # modifiers.remove_modifier('battleships', ModifierType.DEFENCE_MODIFIERS)
-    for el in modifiers.attack_modifiers:
-        print(el['priority'])
     # print('defence', modifiers.defence_modifiers)
