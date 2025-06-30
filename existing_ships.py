9207569236be78457a22eb9572705de4047f3e89
Javlon Talipov <javlon.talipov13@mail.ru>
2025-06-30 17:46:05 +0500

    fix getting right ship in existing_ships.py

--- existing_ships.py
+++ existing_ships.py
@@ -19,7 +19,7 @@ def get_ship_by_name(ship_name):
         data = json.load(file)
         ship_info = data.get(ship_name)
         if not ship_info:
-            raise ShipNameNotFound(f'Ship "{ship_name}" has not found!')
+            raise ShipNameNotFound(ship_name)
         return Ship(**ship_info)
 
 
@@ -35,12 +35,10 @@ def input_validated_ship(msg):
             ship = get_ship_by_name(input(msg).strip())
             set_ship_position(ship)
             return ship
-        except ShipNameNotFound as e:
-            raise ShipNameNotFound(f'Ship "{e}" has not found!')
         except Exception as e:
-            raise Exception(e)
+            print(f'Ship "{e}" has not found!')
 
 
 if __name__ == "__main__":
-    ship_1 = get_ship_by_name('Belfast')
+    ship_1 = get_ship_by_name('Belf2ast')
     print(ship_1.modifiers)
