0665176069189881e4272f427a395f3239ab144a
Javlon Talipov <javlon.talipov13@mail.ru>
2025-09-06 16:35:27 +0500

    add types in ship.py

--- ship/ship.py
+++ ship/ship.py
@@ -1,9 +1,10 @@
-from ship.modifiers.modifiers import Modifiers
-from ship.util_classes import Coords, Azimuth
 from itertools import chain
 
+from ship.modifiers.modifiers import Modifiers
+from ship.util_classes import Coords, Azimuth
 
-def get_row_format():
+
+def get_row_format() -> tuple[str, str]:
     """
     Функция расчитана для построения таблицы вывода информации для экземпляров класса Ship.
 
@@ -28,7 +29,7 @@ def get_row_format():
         "Азимут": 6,
     }
     template = "".join(["{:<" + str(cols[key]) + "}" for key in cols])
-    return (template, template.format(*cols.keys()))
+    return template, template.format(*cols.keys())
 
 
 class Ship:
@@ -47,7 +48,17 @@ class Ship:
 
     _format_template, _formated_titles = get_row_format()
 
-    def __init__(self, name, ship_type, nation, damage, attack_range, hp, velocity, modifiers):
+    def __init__(
+            self,
+            name: str,
+            ship_type: str,
+            nation: str,
+            damage: float,
+            attack_range: float,
+            hp: float,
+            velocity: float,
+            modifiers: dict[str, list[str]]
+    ):
         self.name = name
         self.ship_type = ship_type
         self.nation = nation
@@ -55,12 +66,12 @@ class Ship:
         self.attack_range = attack_range
         self.hp = hp
         self.velocity = velocity
-        self.coords = Coords(0, 0)
-        self.azimuth = Azimuth(0)
+        self.coords = Coords(0.0, 0.0)
+        self.azimuth = Azimuth(0.0)
         self.modifiers = Modifiers(modifiers)
 
     @staticmethod
-    def apply_modifiers(attacker, attacked):
+    def apply_modifiers(attacker: "Ship", attacked: "Ship"):
         """
         Применяет модификаторы атакующего (attacker), а после модификаторы атакуемого (attacked) 
         к рассчету урона атакующего
@@ -78,14 +89,14 @@ class Ship:
     def change_coords(self):
         self.coords.change_coords(self.velocity, self.azimuth.azimuth_in_rad)
 
-    def set_coords(self, x, y):
+    def set_coords(self, x: float, y: float):
         self.coords.x = x
         self.coords.y = y
 
-    def set_azimuth(self, azimuth):
+    def set_azimuth(self, azimuth: float):
         self.azimuth.set_azimuth(azimuth)
 
-    def get_distance_between(self, ship):
+    def get_distance_between(self, ship: "Ship") -> float:
         """
         Функция нахождения расстояние между двумя точками по их координатам, если заданы две точки:
             A с координатами (x1, y1)
@@ -97,10 +108,10 @@ class Ship:
                     ** 2 + (self.coords.y - ship.coords.y) ** 2) ** 0.5
         return round(distance, 2)
 
-    def can_attack(self, ship):
+    def can_attack(self, ship: "Ship") -> bool:
         return self.get_distance_between(ship) <= self.attack_range
 
-    def attack(self, ship):
+    def attack(self, ship: "Ship"):
         if not self.can_attack(ship):
             # print(f'{ship.name} is too far (distance is {self.get_distance_between(ship): .02f} when expecting {self.attack_range}) or {self.name} was destroyed ({self.name} healthpoint is {self.hp})')
             pass
@@ -110,19 +121,19 @@ class Ship:
                 f'{self.name} hit {ship.name} with {total_damage} damage.')
             ship.receive_damage(total_damage)
 
-    def is_alive(self):
+    def is_alive(self) -> bool:
         return self.hp > 0
 
-    def receive_damage(self, damage):
-        self.hp = max(0, self.hp - damage)
+    def receive_damage(self, damage: float):
+        self.hp = max(0.0, self.hp - damage)
         if not self.is_alive():
             print(f'{self.name} has been destroyed!')
 
-    def __str__(self):
+    def __str__(self) -> str:
         return Ship._formated_titles + "\n" + self._get_row
 
     @property
-    def _get_row(self):
+    def _get_row(self) -> str:
         return Ship._format_template.format(
             self.name, self.ship_type, self.nation,
             str(self.damage), str(self.attack_range),
