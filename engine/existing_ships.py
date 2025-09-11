0c639c46787ad9206b741cf136a2d28d6d9139d8
Javlon Talipov <javlon.talipov13@mail.ru>
2025-09-11 19:55:00 +0500

    add get_ship fn to existing_ships.py

--- engine/existing_ships.py
+++ engine/existing_ships.py
@@ -2,8 +2,6 @@ import json
 import pathlib
 from typing import Any
 
-import aiofiles
-
 from engine.game_errors import ShipNameNotFound
 from engine.set_ship_position import set_ship_position
 from engine.ship import Ship
@@ -18,18 +16,7 @@ def get_ship_by_name(ship_name: str) -> Ship:
         ship_info = data.get(ship_name)
         if not ship_info:
             raise ShipNameNotFound(ship_name)
-        return Ship(**ship_info)
-
-
-async def get_ship_by_name_api(ship_name: str) -> dict[str, Any]:
-    async with aiofiles.open(path_to_ships, encoding='UTF-8') as file:
-        content = await file.read()  # Read file content as string
-        data = json.loads(content)  # Parse
-        ship_name = ship_name.lower()
-        ship_info = data.get(ship_name)
-        if not ship_info:
-            raise ShipNameNotFound(ship_name)
-        return ship_info
+        return get_ship(ship_info)
 
 
 def input_validated_ship(msg: str) -> Ship:
@@ -40,6 +27,12 @@ def input_validated_ship(msg: str) -> Ship:
             return ship
         except Exception as e:
             print(f'Ship "{e}" has not found!')
+
+
+def get_ship(ship_data: dict[str, Any]) -> Ship:
+    return Ship(
+        **ship_data
+    )
 
 
 if __name__ == "__main__":
